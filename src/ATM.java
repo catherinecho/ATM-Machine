265a4dc82419e76712060498fd63029f4968fb3d
catherine cho <ccho2@hwemail.com>
2022-08-30 14:05:46 -0700

    atm first code

---
+++ src/ATM.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+public class ATM {
+	static HashMap<Integer, Double> accounts;
+	static double balance = 0.0;
+	public static void main(String[] args) {
+		accounts = new HashMap<Integer,Double>();
+	}
+	
+	public static void openAccount(int num) {
+		accounts.put(num, 0.0);
+	}
+	public static void openAccount(int num, double mon) {
+		if(accounts.containsKey(num)) {
+			accounts.put(num, accounts.get(num) + mon);
+		}else {
+			accounts.put(num, mon);
+		}
+	}
+	public static void closeAccount(int num ) {
+		accounts.remove(num);
+	}
+	public static double checkBalance(int num) {
+		return accounts.get(num);
+	}
+	public static boolean depositMoney(int num, double mon) {
+		if(accounts.containsKey(num)) {
+			accounts.put(num, accounts.get(num) + mon);
+			return true;
+		}
+		
+		return false;
+	}
+	public static boolean withdrawMoney(int num, double mon) {
+		if(accounts.containsKey(num) && accounts.get(num)-mon >=0 ) {
+			accounts.put(num, accounts.get(num) - mon);
+			return true;
+		}
+		return false;
+	}
+
+}
+
