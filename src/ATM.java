048a07a6978e2e04b64f676391a985c0b82eb191
catherine cho <ccho2@hwemail.com>
2022-08-31 22:03:11 -0700

    Used the tester
    ran test cases and fixed the rounding errors

--- src/ATM.java
+++ src/ATM.java
@@ -1,10 +1,12 @@
 import java.util.*;
+import java.text.DecimalFormat;
 import java.io.*;
 public class ATM {
-	static HashMap<Integer, Double> accounts;
+	static HashMap<Integer, Double> accounts = accounts = new HashMap<Integer,Double>();
+	private static final DecimalFormat dfZero = new DecimalFormat("0.00");
 	static double balance = 0.0;
 	public static void main(String[] args) {
-		accounts = new HashMap<Integer,Double>();
+		
 		/*
 		openAccount(5);
 		System.out.println(checkBalance(5));
@@ -29,7 +31,10 @@ public class ATM {
 		accounts.remove(num);
 	}
 	public static double checkBalance(int num) {
-		return accounts.get(num);
+		if(accounts.containsKey(num)) {
+			return  Double.parseDouble(dfZero.format(accounts.get(num)));
+		}
+		return 0.0;
 	}
 	public static boolean depositMoney(int num, double mon) {
 		if(accounts.containsKey(num)) {
@@ -40,7 +45,7 @@ public class ATM {
 		return false;
 	}
 	public static boolean withdrawMoney(int num, double mon) {
-		if(accounts.containsKey(num) && accounts.get(num)-mon >=0 ) {
+		if(accounts.containsKey(num) && mon <= accounts.get(num) && mon >= 0) {
 			accounts.put(num, accounts.get(num) - mon);
 			return true;
 		}
